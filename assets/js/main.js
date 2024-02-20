aa93f1e290c0bd7238946c87b595e6490bf744a2
simone-nolberto <simone.nolberto@outlook.com>
2024-02-20 16:04:31 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,30 @@
 
-let userName = document.getElementById("userId")
-let userMileage = document.getElementById("userTRIP");
-
-console.log(userMileage.value);
-console.log(userName.value);
+let userName = document.getElementById("userID");
+let userKm = document.getElementById("userTRIP");
 
 
+const trigger = document.querySelector(".btn")
+trigger.addEventListener('click',
 
-
-let euro = Intl.NumberFormat('en-DE', {
-    style: 'currency',
-    currency: 'EUR',
-});
+    function (userinfo) {
+        console.log(userName.value);
+        console.log(userKm.value);
+    }
+);
 
 const mileagePrice = 0.21;
-const underAgeDiscount = 0.16;
-const overDiscount = 0.12;
+console.log(mileagePrice);
+
+
+
+// let euro = Intl.NumberFormat('en-DE', {
+//     style: 'currency',
+//     currency: 'EUR',
+// });
+
+
+// const underAgeDiscount = 0.16;
+// const overDiscount = 0.12;
 
 
 // if (userAge < 18){
