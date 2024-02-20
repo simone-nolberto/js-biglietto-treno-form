dc913ff32bfdaf270c1a26f0c90d2fd030f33173
simone-nolberto <simone.nolberto@outlook.com>
2024-02-20 17:14:22 +0100

    update
    main working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,41 @@
-
 let userName = document.getElementById("userID");
 let userKm = document.getElementById("userTRIP");
-
-
-const trigger = document.querySelector(".btn")
-trigger.addEventListener('click',
-
-    function (userinfo) {
-        console.log(userName.value);
-        console.log(userKm.value);
-    }
-);
+let userAge = document.getElementById("userAGE");
 
 const mileagePrice = 0.21;
-console.log(mileagePrice);
+const underAgeDiscount = 0.16;
+const overDiscount = 0.12;
+
+let euro = Intl.NumberFormat('en-DE', {
+    style: 'currency',
+    currency: 'EUR',
+});
+
+const trigger = document.querySelector(".btn")
+trigger.addEventListener('click', function () {
+
+    let km = parseInt(userKm.value);
+    let age = parseInt(userAge.value)
+    console.log(userName.value);
+    console.log(km);
+    console.log(age);
+
+
+    if (age < 18) {
+
+        console.log(euro.format(km * underAgeDiscount));
+
+    } else if (age >= 65) {
+
+        console.log(euro.format(km * overDiscount));
+
+    } else {
+        console.log(euro.format(km * mileagePrice));
+    }
+
+
+});
+
 
 
 
@@ -23,8 +45,7 @@ console.log(mileagePrice);
 // });
 
 
-// const underAgeDiscount = 0.16;
-// const overDiscount = 0.12;
+
 
 
 // if (userAge < 18){
